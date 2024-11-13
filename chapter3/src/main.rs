d4b166438a3b2c4950a89ca402614f955ff8fc54
admin <gyf@travelsky.com.cn>
2024-11-13 22:48:35 +0800

    update

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,6 +1,10 @@
 mod image_concat;
+mod create_track_bar;
 
 fn main() {
     // 1. image concat
-    let _ = image_concat::run();
+    // let _ = image_concat::run();
+
+    // 2. create track bar
+    let _ = create_track_bar::run();
 }
