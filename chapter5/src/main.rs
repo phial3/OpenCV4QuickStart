a0556444691b115b156844a69c102f8e8605338d
admin <gyf@travelsky.com.cn>
2024-11-15 10:49:02 +0800

    add box_filter

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -1,10 +1,14 @@
 mod bilateral_filter;
 mod image_blur;
+mod box_filter;
 
 fn main() {
     // 1. bilateral filter
     // let _ = bilateral_filter::run();
 
     // 2. image blur
-    let _ = image_blur::run();
+    // let _ = image_blur::run();
+
+    // 3. box filter
+    let _ = box_filter::run();
 }
